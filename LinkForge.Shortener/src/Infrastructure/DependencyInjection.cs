a77be8329c6ee3605f438d604018bbcc68f9b65e
Leonardo Lima <leonardolima.art@gmail.com>
2026-05-15 16:54:36 -0400

    feat(infra): add shortlink repository

--- LinkForge.Shortener/src/Infrastructure/DependencyInjection.cs
+++ LinkForge.Shortener/src/Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
+using Application.Abstractions;
 using Infrastructure.Persistence;
+using Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +12,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<AppDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+        services.AddScoped<IShortLinkRepository, ShortLinkRepository>();
         return services;
     }
 }
