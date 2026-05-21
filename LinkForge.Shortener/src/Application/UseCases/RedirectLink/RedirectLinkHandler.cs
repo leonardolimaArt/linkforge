68f0cc30bf8d2fd961974f2e2f38b6782e3a0f78
Leonardo Lima <leonardolima.art@gmail.com>
2026-05-21 16:22:59 -0400

    feat(application): add logging for redis in redirect link handler

--- LinkForge.Shortener/src/Application/UseCases/RedirectLink/RedirectLinkHandler.cs
+++ LinkForge.Shortener/src/Application/UseCases/RedirectLink/RedirectLinkHandler.cs
@@ -1,15 +1,19 @@
 using Application.Abstractions;
+using Microsoft.Extensions.Logging;
 
 namespace Application.UseCases.RedirectLink;
 
-public class RedirectLinkHandler(IShortLinkRepository repository, ILinkCache cache)
+public class RedirectLinkHandler(IShortLinkRepository repository, ILinkCache cache, ILogger<RedirectLinkHandler> logger)
 {
     public async Task<string?> HandleAsync(RedirectLinkQuery query, CancellationToken cancellationToken = default)
     {
         var cached = await cache.GetAsync(query.ShortCode, cancellationToken);
-        if(cached is not null)
+        if(cached is not null){
+            logger.LogInformation("Cache HIT for {shortCode}", query.ShortCode);
             return cached;
+        }
 
+        logger.LogInformation("Cache MISS for {shortCode}", query.ShortCode);
         var shortLink = await repository.GetByShortCodeAsync(query.ShortCode, cancellationToken);
         if(shortLink is null)
             return null;
