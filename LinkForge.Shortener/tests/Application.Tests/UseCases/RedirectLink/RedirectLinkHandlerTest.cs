aa1eac47fae48c32019e5b30ebde109d2004c201
Leonardo Lima <leonardolima.art@gmail.com>
2026-05-21 17:15:49 -0400

    fix(tests): add redis and authorization pass adjust

--- LinkForge.Shortener/tests/Application.Tests/UseCases/RedirectLink/RedirectLinkHandlerTest.cs
+++ LinkForge.Shortener/tests/Application.Tests/UseCases/RedirectLink/RedirectLinkHandlerTest.cs
@@ -2,6 +2,7 @@ using Application.Abstractions;
 using Application.UseCases.RedirectLink;
 using Domain.ShortLink;
 using FluentAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
 using NSubstitute;
 
 namespace Application.Tests.UseCases.RedirectLink;
@@ -11,11 +12,13 @@ public class RedirectLinkHandlerTests
 {
     private readonly IShortLinkRepository _repository;
     private readonly RedirectLinkHandler _handler;
+    private readonly ILinkCache _cache;
 
     public RedirectLinkHandlerTests()
     {
         _repository = Substitute.For<IShortLinkRepository>();
-        _handler = new RedirectLinkHandler(_repository);
+        _cache = Substitute.For<ILinkCache>();
+        _handler = new RedirectLinkHandler(_repository, _cache, NullLogger<RedirectLinkHandler>.Instance);
     }
 
     [Fact]
@@ -23,6 +26,7 @@ public class RedirectLinkHandlerTests
     {
         var shortLink = ShortLink.Create(new OriginalUrl("https://google.com"), new ShortCode("abcd1234"), DateTimeOffset.UtcNow);
 
+        _cache.GetAsync("abcd1234", Arg.Any<CancellationToken>()).Returns((string?)null);
         _repository.GetByShortCodeAsync("abcd1234", Arg.Any<CancellationToken>()).Returns(shortLink);
 
         var query = new RedirectLinkQuery("abcd1234");
@@ -35,6 +39,7 @@ public class RedirectLinkHandlerTests
     [Fact]
     public async Task Should_Return_Null_When_ShortCode_NotExists()
     {
+        _cache.GetAsync(Arg.Any<string>(), Arg.Any<CancellationToken>()).Returns((string?)null);
         _repository.GetByShortCodeAsync(Arg.Any<string>(), Arg.Any<CancellationToken>()).Returns((ShortLink?)null);
 
         var query = new RedirectLinkQuery("inexistente");
