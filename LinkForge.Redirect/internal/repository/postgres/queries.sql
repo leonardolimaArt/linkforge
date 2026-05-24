b07bbb6c6de91e6f1499ebca412b43ec335d84a4
Leonardo Lima <leonardolima.art@gmail.com>
2026-05-24 11:13:11 -0400

    feat(redirect): add upsert in repository for event driven sync

--- LinkForge.Redirect/internal/repository/postgres/queries.sql
+++ LinkForge.Redirect/internal/repository/postgres/queries.sql
@@ -2,3 +2,8 @@
 SELECT id, original_url, short_code, created_at
 FROM short_links
 WHERE short_code = $1;
+
+-- name: UpsertShortLink :exec
+INSERT INTO short_links (id, original_url, short_code, created_at)
+VALUES ($1, $2, $3, $4)
+ON CONFLICT (short_code) DO NOTHING;
