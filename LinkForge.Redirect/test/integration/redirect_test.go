d53ecbd498d13eaf751dab4f3e98e0de271dcc0e
Leonardo Lima <leonardolima.art@gmail.com>
2026-05-29 23:54:25 -0400

    tests(redirect): add kafka integrations coverage tests

--- LinkForge.Redirect/test/integration/redirect_test.go
+++ LinkForge.Redirect/test/integration/redirect_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/testcontainers/testcontainers-go"
 	pgcontainer "github.com/testcontainers/testcontainers-go/modules/postgres"
 	rediscontainer "github.com/testcontainers/testcontainers-go/modules/redis"
+	"github.com/testcontainers/testcontainers-go/modules/redpanda"
 	"github.com/testcontainers/testcontainers-go/wait"
 
 	"github.com/leonardolimaArt/linkforge/LinkForge.Redirect/internal/cache"
@@ -42,9 +43,10 @@ CREATE TABLE short_links (
 `
 
 var (
-	testPool   *pgxpool.Pool
-	testRedis  *redis.Client
-	testRouter *gin.Engine
+	testPool         *pgxpool.Pool
+	testRedis        *redis.Client
+	testRouter       *gin.Engine
+	testKafkaBrokers []string
 )
 
 type noopOrigin struct{}
@@ -83,6 +85,22 @@ func TestMain(m *testing.M) {
 		fmt.Printf("failed to start redis: %v\n", err)
 		os.Exit(1)
 	}
+
+	rpC, err := redpanda.Run(ctx,
+		"redpandadata/redpanda:v24.2.4",
+		redpanda.WithAutoCreateTopics(),
+	)
+	if err != nil {
+		fmt.Printf("failed to start redpanda: %v\n", err)
+		os.Exit(1)
+	}
+
+	seedBroker, err := rpC.KafkaSeedBroker(ctx)
+	if err != nil {
+		fmt.Printf("failed to get kafka seed broker: %v\n", err)
+		os.Exit(1)
+	}
+	testKafkaBrokers = []string{seedBroker}
 
 	redisHost, err := redisC.Host(ctx)
 	if err != nil {
@@ -135,6 +153,7 @@ func TestMain(m *testing.M) {
 	testRedis.Close()
 	_ = pgC.Terminate(ctx)
 	_ = redisC.Terminate(ctx)
+	_ = rpC.Terminate(ctx)
 
 	os.Exit(code)
 
